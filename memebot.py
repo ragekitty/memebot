357f2cced0ba553a984240b6a19a6b6448bd760e
Breadsauce <nick.b.bradshaw@gmail.com>
2022-12-07 16:06:01 -0600

    memebot.py

---
+++ memebot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import asyncpraw
+import asyncio
+import random
+
+# Create a new Discord client
+intents = discord.Intents().all()
+intents.members = True
+client = discord.Client(intents=intents)
+
+reddit = asyncpraw.Reddit(
+    client_id="xxx",
+    client_secret="xxx",
+    user_agent="Discord_bot",
+    aiohttp_kwargs={"timeout": 10},
+)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    # meme me daddy
+    reddit = asyncpraw.Reddit(
+        client_id="oid8ZUYxYTA7TmgCWDN1RQ",
+        client_secret="cvTuSNwJl3i8-6m1Q9evk2_KLzZBLg",
+        user_agent="Discord_bot",
+        aiohttp_kwargs={"timeout": 10},
+    )
+    list = []
+    subreddit = await reddit.subreddit("memes")
+    async for submission in subreddit.hot(limit=50):
+            list.append(submission)
+    await message.channel.send(random.choice(list).url)
+
+# Run the bot
+client.run('discord api goes here')
